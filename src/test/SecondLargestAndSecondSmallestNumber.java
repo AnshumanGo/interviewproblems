75016712801c294a7da2a328ece70a4a341f66e7
AnshumanGo <anshuman.g.cs@gmail.com>
2021-01-16 23:38:44 +0530

    solution for second largest and second smallest number in array without changing array

---
+++ src/test/SecondLargestAndSecondSmallestNumber.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class SecondLargestAndSecondSmallestNumber {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {9,2,6,5,7,9,2};
+        Arrays.sort(intArray);
+
+        if(intArray.length==1)
+        {
+            System.out.println("Second largest number in array" + intArray[0]);
+            System.out.println("Second smallest number in array" + intArray[0]);
+        }else if (intArray.length == 2){
+            System.out.println("Second largest number in array" + intArray[0]);
+            System.out.println("Second smallest number in array" + intArray[1]);
+        }else {
+
+            TreeSet<Integer> integerTreeSet = new TreeSet<>();
+            for (int i =0 ; i< intArray.length;i++) {
+                integerTreeSet.add(intArray[i]);
+            }
+
+            int[] intArrayUnique = new int[integerTreeSet.size()];
+            int intArrayUniqueIndex =0;
+            for (int i : integerTreeSet) {
+
+                intArrayUnique[intArrayUniqueIndex++]=i;
+            }
+
+            System.out.println("Second largest number in array" + intArrayUnique[1]);
+            System.out.println("Second largest number in array" + intArrayUnique[intArrayUnique.length-2]);
+
+            integerTreeSet.pollFirst();
+            integerTreeSet.pollLast();
+
+            System.out.println("Second largest number in array" + integerTreeSet.first());
+            System.out.println("Second largest number in array" + integerTreeSet.last());
+        }
+    }
+}

39d5f4949f91ac3e8b24831d82349c0b789f304a
AnshumanGo <anshuman.g.cs@gmail.com>
2025-04-06 08:57:52 +0530

    6th April 2025 part 1

---
+++ src/com/problems/java/FindFirstCommonElementInTwoList.java
@@ -0,0 +1,42 @@
+package com.problems.java;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FindFirstCommonElementInTwoList {
+    static Integer commonElement (List<Integer> al1, List<Integer> al2){
+
+        Integer result = null;
+
+
+        Optional<Integer> first = al1.stream().filter(e -> al2.contains(e)).findFirst();
+
+        if(first.isPresent()){
+            result = first.get();
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        List<Integer> al1 = new ArrayList<>();
+        List<Integer> al2 = new ArrayList<>();
+        al1.add(1);
+        al1.add(2);
+        al1.add(3);
+        al1.add(4);
+        al1.add(5);
+
+        al2.add(1);
+        al2.add(3);
+        al2.add(4);
+        al2.add(3);
+
+        Integer val = commonElement(al1, al2);
+        if(val != null){
+            System.out.println(val.intValue());
+        }
+
+    }
+
+}
